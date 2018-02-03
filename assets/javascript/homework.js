36c2acb59bb64dc3c8b449f3714fb71dcec399ae
Mary Doucet <marydoucet@Marys-MacBook-Pro.local>
2018-02-02 21:24:49 -0500

    done

--- assets/javascript/homework.js
+++ assets/javascript/homework.js
@@ -4,6 +4,7 @@
 
 
 
+
   // Initialize Firebase
     var config = {
     apiKey: "AIzaSyDySbzKfm4vcDdeZMDZT5M8y8U53E6ULbE",
@@ -13,92 +14,82 @@
     storageBucket: "traintime-70c8c.appspot.com",
     messagingSenderId: "223788944954"
   };
+
   firebase.initializeApp(config);
 
- var database=firebase.database();
- var currentTime = moment();
+var database = firebase.database();
+var currentTime = moment();
 
+database.ref().on("child_added", function(childSnap) {
 
-database.ref().on("child_added" , function(childSnapshot){
-    
-    
+    var name = childSnap.val().name;
+    var destination = childSnap.val().destination;
+    var firstTrain = childSnap.val().firstTrain;
+    var frequency = childSnap.val().frequency;
+    var min = childSnap.val().min;
+    var next = childSnap.val().next;
 
-      var trainName = childSnapshot.val().trainName;
-      var destination = childSnapshot.val().destination;
-      var firstTrainTime = childSnapshot.val().firstTrainTime;
-      var frequency = childSnapshot.val().frequency;
-      var min = childSnapshot.val().min;
-      var next = childSnapshot.val().next;
+    $(".table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
 
-   $("tbody").append("<tr><td" + name + "</td><td>" + destination + "</td><td>" + time + "</td><td>" 
-            + frequency + "</td><td>" + min + "</td><td>" + next + "</td></tr>");
- 
-
-
-   });
+database.ref().on("value", function(snapshot) {
    
-database.ref().on("value", function(snapshot){
 
-  
+});
 
-   });
+//grabs information from the form
+$("#submit").on("click", function() {
+   event.preventDefault();
 
- 
-$("#submit").on("click", function(){
-      event.preventDefault();
-      var trainName = $("#name").val().trim();
-      var destination = $("#destination").val().trim();
-      var firstTrainTime = $("#firstTrainTime").val().trim();
-      var frequency = $("#frequency").val().trim();
+    var trainName = $("#name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
 
-      if (trainName == ""){
-        alert("Enter a train name.");
+    //ensures that each input has a value
+    if (trainName == "") {
+        alert('Enter a train name.');
         return false;
-      }
-
-      if (destination == ""){
-        alert("Enter a destination.");
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
         return false;
-      }
-
-      if (firstTrainTime == ""){
-        alert("Enter a first train time.");
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
         return false;
-      }
-
-      if(frequency ==""){
-        alert("Enter a frequency");
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
         return false;
-      }
+    }
 
-      var firstTrainConverted = moment(firstTrainTime, "hh:mm").subtract("1, years");
-      var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
-      var remainder = difference % frequency;
-      var minUntilTrain = frequency - remainder;
-      var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+    // THE MATH!
+    //subtracts the first train time back a year to ensure it's before current time.
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+    // the time difference between current time and the first train
+    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
 
-      var newTrain ={
-        trainName: name,
+    var newTrain = {
+        name: trainName,
         destination: destination,
-        firstTrainTime: firstTrainTime,
+        firstTrain: firstTrain,
         frequency: frequency,
         min: minUntilTrain,
         next: nextTrain
+    }
 
+    console.log(newTrain);
+    database.ref().push(newTrain);
 
-      }
-
-      console.log(newTrain);
-      database.ref().push(newTrain);
-  
     $("#name").val("");
     $("#destination").val("");
     $("#firstTrainTime").val("");
     $("#frequency").val("");
-    
 
     return false;
-
-
 });
 });
